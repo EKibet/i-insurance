536cfc0a3b618cdb1f385075d671a2cefecd2af4
becky nayere <rebecca.nayere@gmail.com>
2020-11-17 23:27:37 +0300

    add db details

--- insurance/settings.py
+++ insurance/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = 'insurance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': 'beinsurance',
+        'USER': 'Bryon',
+        'PASSWORD': 'nayere',
         # 'HOST': os.getenv('DB_HOST'),
     #    'PORT': '',
     }
