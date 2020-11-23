76f1f45d96764dfda31cbefbf8b525d1eac7f7be
becky nayere <rebecca.nayere@gmail.com>
2020-11-23 13:45:53 +0300

    config db settings

--- insurance/settings.py
+++ insurance/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOST')
 
 AUTH_USER_MODEL = 'policy.User'
 
-REST_FRSMEWORK={
+REST_FRAMEWORK={
      'NON_FIELDS_ERRORS_KEY':'error',
      'DEFAULT_AUTHENTIFICATION_CLASSES':(
         'rest_framework_simplejwt.policy.JWTAuthentification',
@@ -111,25 +111,29 @@ WSGI_APPLICATION = 'insurance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'beinsurance',
-        'USER': 'Bryon',
-        'PASSWORD': 'nayere',
-        # 'HOST': os.getenv('DB_HOST'),
-    #    'PORT': '',
+if os.getenv('MODE')=="dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': '',
+        }
+        
     }
-}
 # production
-# else:
-#    DATABASES = {
-#        'default': dj_database_url.config(
-#            default=os.getenv('DATABASE_URL')
-#        )
-#    }
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=os.getenv('DATABASE_URL')
+       )
+   }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
